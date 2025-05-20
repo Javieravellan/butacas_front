44d253c3beb835ef175c46d6a0252b3b2f7e390b
Javier Chiquito Avellán <jacha067@gmail.com>
2025-05-19 22:58:18 -0500

    Corrección comportamiento de componente Seat

--- app/components/reservas/Seat.tsx
+++ app/components/reservas/Seat.tsx
@@ -1,37 +1,37 @@
 import { useEffect, useState } from "react";
 
 export const Seat = (props: { items: any[] }) => {
-    const [seatsData, setSeatsData] = useState<any[]>([]);
-
-    useEffect(() => {
-        setSeatsData(props.items?.filter(({ status }) => !status));
-    }, [props.items]);
+    const [selectedSeats, setSelectedSeats] = useState<any[]>([]);
 
     const onSelectSeat = (seat: any) => {
-        const seatIndex = seatsData.findIndex((s: any) => s.id === seat.id);
-        if (seatIndex >= 0) {
-            setSeatsData(seatsData.filter((s: any) => s.id !== seat.id));
-        }
-        else {
-            setSeatsData([...seatsData, { ...seat, status: false }]);
+        if (selectedSeats.some((s) => s.id === seat.id)) {
+            setSelectedSeats(selectedSeats.filter((s) => s.id !== seat.id));
+        } else {
+            setSelectedSeats([...selectedSeats, {...seat, status: false }]);
         }
     };
 
     return (
         <div className="grid grid-cols-3 gap-2 w-full">
-            {props.items?.map((seat: any) => (
-                <button
+            {props.items?.map((seat: any) => {
+                const isReserved = !seat.status;
+                const isSelected = selectedSeats.some((s) => s.id === seat.id);
+
+                return <button
                     key={seat.id}
                     type="button"
-                    onClick={() => onSelectSeat(seat)}
-                    className={`p-2 border rounded-md text-center text-sm ${seatsData?.some((s: any) => s.id === seat.id)
-                        ? 'bg-indigo-100 border-indigo-500 text-indigo-700'
-                        : 'bg-gray-50 hover:bg-gray-100'
+                    onClick={() => !isReserved && onSelectSeat(seat)}
+                    className={`p-2 border rounded-md text-center text-sm
+                            ${isReserved
+                                ? 'bg-gray-300 text-gray-500 cursor-not-allowed'
+                                : isSelected
+                                    ? 'bg-indigo-100 border-indigo-500 text-indigo-700'
+                                    : 'bg-gray-50 hover:bg-gray-100'
                         }`}
                 >
                     {seat.rowNumber}{seat.number}
                 </button>
-            ))}
+            })}
         </div>
     )
 }
