9d1900b5018acb894b834c9829b12bae867abfcf
Javier Chiquito Avellán <jacha067@gmail.com>
2025-05-28 00:09:41 -0500

    creación de formato fecha dia actual

--- app/utils.ts
+++ app/utils.ts
@@ -16,4 +16,18 @@ const onlyHours = (dateString: string) => {
     }
 }
 
-export { formatDate, onlyHours };
+const showTodayOrDateDefault = (dateString: string) => {
+    try {
+        const date = parseISO(dateString);
+        const now = new Date();
+        
+        const isToday = date.getDate() === now.getDate() &&
+               date.getMonth() === now.getMonth() &&
+               date.getFullYear() === now.getFullYear()
+        return isToday ? 'Hoy a las ' + onlyHours(dateString)  : formatDate(dateString);
+    } catch {
+        return false;
+    }
+};
+
+export { formatDate, onlyHours, showTodayOrDateDefault };
